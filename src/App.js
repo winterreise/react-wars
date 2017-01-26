d19d6a00eeb01ecd69aca271341dd5f56883bc1d
Pui Wing Tam <puiwing.tam@gmail.com>
2017-01-25 21:56:11 -0500

    Implemented planet search function, cosmetic CSS changes

--- src/App.js
+++ src/App.js
@@ -8,20 +8,40 @@ class App extends Component {
 
   constructor() {
     super();
+    this.search = this.search.bind(this);
+    this.handleQueryChange = this.handleQueryChange.bind(this);
     this.state = {
       planets: {
         count: 0,
         results: []
-      }
+      },
+      query: ''
     };
   }
 
   componentDidMount() {
-    $.get('http://swapi.co/api/planets')
+    this.getPlanets();
+  }
+
+  getPlanets() {
+    $.get(`http://swapi.co/api/planets?search=${this.state.query}`)
     .then(response => {
       this.setState({
-        planets: response
+        planets: response,
+        query: this.state.query
       });
+    });
+  }
+
+  search(e) {
+    e.preventDefault();
+    this.getPlanets();
+  }
+
+  handleQueryChange(e) {
+    this.setState({
+      planets: this.state.planets,
+      query: e.target.value
     });
   }
 
@@ -31,8 +51,12 @@ class App extends Component {
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Skywalker's Guide to the Galaxy</h2>
+          <form onSubmit={this.search}>
+            <input type="text" onChange={this.handleQueryChange} placeholder="Search for planets"></input>
+            <button type="submit">submit</button>
+          </form>
         </div>
-        <PlanetList planets={this.state.planets}/> 
+        <PlanetList planets={this.state.planets}/>
       </div>
     );
   }
